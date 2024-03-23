f0362f05845a3a32d3fc6a5a2e789bd0f9539314
klyushkin <stepan.9h88@mail.ru>
2024-03-23 14:00:25 +0300

    Add ViewModel and ui

--- app/src/main/java/com/example/room_phone_number_list_app/ContactState.kt
+++ app/src/main/java/com/example/room_phone_number_list_app/ContactState.kt
@@ -1,7 +1,7 @@
 package com.example.room_phone_number_list_app
 
 data class ContactState(
-    val contact: List<Contacts> = emptyList(),
+    val contacts: List<Contacts> = emptyList(),
     val firstName: String = "",
     val lastName: String = "",
     val phoneNumber: String = "",
--- app/src/main/java/com/example/room_phone_number_list_app/MainActivity.kt
+++ app/src/main/java/com/example/room_phone_number_list_app/MainActivity.kt
@@ -3,21 +3,47 @@ package com.example.room_phone_number_list_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.room.Room
 import com.example.room_phone_number_list_app.ui.theme.Room_phone_number_list_appTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            ContactsDatabase::class.java,
+            "contacts.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<ContactViewModel> {
+        viewModelFactory {
+            object : ViewModelProvider.Factory{
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return ContactViewModel(db.dao) as T
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Room_phone_number_list_appTheme {
-
+                val state by viewModel.state.collectAsState()
+                ContactScreen(state = state, onEvent = viewModel::onEvent)
             }
         }
     }
